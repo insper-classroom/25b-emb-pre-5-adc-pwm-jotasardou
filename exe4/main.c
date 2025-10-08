a37fa6dc2f76f4907b8f0a38c1965b39401c4979
João Pedro Sardou de Oliveira <joaopso2@al.insper.edu.br>
2025-10-08 19:59:43 -0300

    Primeira tentativa ex 4

--- exe4/main.c
+++ exe4/main.c
@@ -18,16 +18,16 @@
 #include "hardware/adc.h"
  
 const int PIN_LED_B = 4;
-volatile int flag_timer_0 = 0;
-volatile int flag_timer_1 = 1;
+volatile int flag_timer_zona_1 = 0;
+volatile int flag_timer_zona_2 = 0;
 
 bool timer_0_callback(repeating_timer_t *rt) {
-    flag_timer_0 = 1;
+    flag_timer_zona_1 = 1;
     return true; // keep repeating
 }
 
 bool timer_1_callback(repeating_timer_t *rt) {
-    flag_timer_1 = 1;
+    flag_timer_zona_2 = 1;
     return true; // keep repeating
 }
 
@@ -36,7 +36,7 @@ int main() {
     adc_init(); 
 
     adc_gpio_init(28);
-    adc_select_input(1);
+    
 
     gpio_init(PIN_LED_B);
     gpio_set_dir(PIN_LED_B, GPIO_OUT);
@@ -44,24 +44,57 @@ int main() {
     repeating_timer_t timer_0;
     add_repeating_timer_ms(300, timer_0_callback, NULL, &timer_0);
     add_repeating_timer_ms(500, timer_1_callback, NULL, &timer_0);
+    
+    int led_state = 0;
+    // int zona =0;
+
+    
+
+
     while (1) {
-        const float conversion_factor = 3.3f / (1 << 12);
+        adc_select_input(2);
         uint16_t result = adc_read();
+        const float conversion_factor = 3.3f / (1 << 12);
+
         printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor); 
 
-        if (result * conversion_factor >= 0 || conversion_factor < 1){
+        if (result * conversion_factor >= 0 && result * conversion_factor < 1){
+            printf("Zona 0 \n");
             // desligado
+            // zona = 0;
             gpio_put(PIN_LED_B, 0);
         }
-        else if (result * conversion_factor >= 1 || result * conversion_factor < 2){
+        else if (result * conversion_factor >= 1 && result * conversion_factor < 2){
+            printf("Zona 1 \n");
+            if (flag_timer_zona_1 == 1) {
+                flag_timer_zona_1 = 0;
+                led_state = !led_state;
+                gpio_put(PIN_LED_B, led_state);
+            } 
             // timer de 300ms
-            if (flag_timer_0 == 1){
-                
-                gpio_put(PIN_LED_B, 1);
-            }
+            
         }
-        else{
+        else if(result * conversion_factor >= 2 && result * conversion_factor < 3.3){
+            printf("Zona 2 \n");
+            if (flag_timer_zona_2 == 1) {
+                flag_timer_zona_2 = 0;
+                led_state = !led_state;
+                gpio_put(PIN_LED_B, led_state);
+            } 
             // timer de 500ms
         }
+
+        // if(zona == 1 && flag_timer_zona_1 == 1) {
+        //     flag_timer_zona_1 = 0;
+        //     // gpio put
+        // } 
+
+        // if(zona == 2 && flag_timer_zona_2 == 1) {
+        //     flag_timer_zona_2 = 0;
+        // }
+
+        // if (zona == 0) {
+        //     // apaga o led
+        // }
     }
 }
