a5aa338770e8848638b84b99fd89fc6831479819
João Pedro Sardou de Oliveira <joaopso2@al.insper.edu.br>
2025-10-08 10:25:32 -0300

    Ex2 done

--- exe2/main.c
+++ exe2/main.c
@@ -8,14 +8,24 @@
 #include "hardware/pwm.h"
 
 const int PWM_0_PIN = 4;
+const int PWM_1_PIN = 6;
 
 void led_task(void *p) {
     gpio_set_function(PWM_0_PIN, GPIO_FUNC_PWM);
+    gpio_set_function(PWM_1_PIN, GPIO_FUNC_PWM);
+
     uint slice_num = pwm_gpio_to_slice_num(PWM_0_PIN);
+    uint slice_num1 = pwm_gpio_to_slice_num(PWM_1_PIN);
+
     pwm_set_clkdiv(slice_num, 125);
     pwm_set_wrap(slice_num, 100);
     pwm_set_chan_level(slice_num, PWM_CHAN_A, 80);
     pwm_set_enabled(slice_num, true);
+
+    pwm_set_clkdiv(slice_num1, 125);
+    pwm_set_wrap(slice_num1, 100);
+    pwm_set_chan_level(slice_num1, PWM_CHAN_A, 20);
+    pwm_set_enabled(slice_num1, true);
 
     while (true) {
     }
