80932446a7b3cc3d413c4d941df8f3b01a089031
João Pedro Sardou de Oliveira <joaopso2@al.insper.edu.br>
2025-10-08 20:05:04 -0300

    Segunda tentativa ex4

--- exe4/main.c
+++ exe4/main.c
@@ -42,8 +42,9 @@ int main() {
     gpio_set_dir(PIN_LED_B, GPIO_OUT);
 
     repeating_timer_t timer_0;
+    repeating_timer_t timer_1;
     add_repeating_timer_ms(300, timer_0_callback, NULL, &timer_0);
-    add_repeating_timer_ms(500, timer_1_callback, NULL, &timer_0);
+    add_repeating_timer_ms(500, timer_1_callback, NULL, &timer_1);
     
     int led_state = 0;
     // int zona =0;
@@ -54,18 +55,16 @@ int main() {
     while (1) {
         adc_select_input(2);
         uint16_t result = adc_read();
-        const float conversion_factor = 3.3f / (1 << 12);
-
-        printf("Raw value: 0x%03x, voltage: %f V\n", result, result * conversion_factor); 
+        const float conversion_factor = 3.3f / (1 << 12); 
 
         if (result * conversion_factor >= 0 && result * conversion_factor < 1){
-            printf("Zona 0 \n");
+            // printf("Zona 0 \n");
             // desligado
             // zona = 0;
             gpio_put(PIN_LED_B, 0);
         }
         else if (result * conversion_factor >= 1 && result * conversion_factor < 2){
-            printf("Zona 1 \n");
+            // printf("Zona 1 \n");
             if (flag_timer_zona_1 == 1) {
                 flag_timer_zona_1 = 0;
                 led_state = !led_state;
@@ -75,7 +74,7 @@ int main() {
             
         }
         else if(result * conversion_factor >= 2 && result * conversion_factor < 3.3){
-            printf("Zona 2 \n");
+            // printf("Zona 2 \n");
             if (flag_timer_zona_2 == 1) {
                 flag_timer_zona_2 = 0;
                 led_state = !led_state;
